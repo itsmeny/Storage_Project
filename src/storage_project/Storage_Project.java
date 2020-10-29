bf693fd8bfab02cd74797da6972fc9a9e89ce829
itsmeny <chokjaroenp501@gmail.com>
2020-10-29 23:17:46 +0700

    Second Commit

--- src/storage_project/Storage_Project.java
+++ src/storage_project/Storage_Project.java
@@ -3,6 +3,6 @@ package storage_project;
 public class Storage_Project {
 
     public static void main(String[] args) {
-        Storage Storage_Rental = new Storage();
+        new Storage();
     }
 }
